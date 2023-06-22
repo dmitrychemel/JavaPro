5512921ca83e4217a13a0bca64db49252a88cd4e
Dzmitry.Chemel <chemel80@gmail.com>
2023-06-22 22:07:06 +0200

    Lesson10 - stack

--- src/lesson10/House.java
+++ src/lesson10/House.java
@@ -1,12 +1,13 @@
 package lesson10;
 
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class House {
     public static void main(String[] args) {
 
-        Queue<Device> device = new ArrayDeque<>();
+        Queue<Device> device = new PriorityQueue<>();
         device.add(new Device("Холодильник", "NORMAL", "Работает"));
         device.add(new Device("Телевизор", "EMERGENCY", "что-то не то"));
         device.add(new Device("Пульт", "NORMAL", "Работает"));
