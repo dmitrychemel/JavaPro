51db810563dba69f5967ba2fc4affaf8f4f1a366
Dzmitry.Chemel <chemel80@gmail.com>
2023-06-25 18:24:54 +0200

    Homework 12 - Stack 25.06.2023

---
+++ src/homework12/CheckBracket.java
@@ -0,0 +1,43 @@
+package homework12;
+import java.util.Stack;
+
+/**
+ * Java Pro. Homework #12
+ * @author Dzmitry Chemel
+ * @version 25 June 2023
+ */
+
+public class CheckBracket {
+    public static void main(String[] args) {
+        String example1 = "(2 + 3) * (5 - 2)";
+        String example2 = "(2 + 3 * (5 - 2)";
+        String example3 = "2 + 3) * (5 - 2";
+
+        System.out.println(checkBrackets(example1));
+        System.out.println(checkBrackets(example2));
+        System.out.println(checkBrackets(example3));
+    }
+
+    public static String checkBrackets(String example){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: example.toCharArray()){
+            if(ch == '('){
+                if(stack.isEmpty() || stack.pop() == ')'){
+                    stack.push(ch);
+                } else {
+                    return "Ошибка: не закрыта скобка";
+                }
+            }
+            if (ch == ')'){
+                if(stack.isEmpty() || stack.pop() != '('){
+                    return "Ошибка: не открыта скобка";
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+
+        return "Скобки расставлены правильно";
+    }
+}
