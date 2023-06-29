de9e6950527c6b70397ae07fac6f6d7a34c01a61
Dzmitry.Chemel <chemel80@gmail.com>
2023-06-29 21:08:10 +0200

    Lesson 12 - 29.06.2023

---
+++ src/lessons12/Exercises.java
@@ -0,0 +1,39 @@
+package lessons12;
+
+import java.util.TreeMap;
+
+public class Exercises {
+
+    static final String TEXT = "привет я человек что ты тут делаешь";
+    static final String SPACE = " ";
+
+    public static void main(String[] args) {
+        TreeMap<String, String> translationRussian = new TreeMap<>();
+        translationRussian.put("привет", "Hello");
+        translationRussian.put("я", "I'm");
+        translationRussian.put("человек", "human");
+        translationRussian.put("что", "what");
+        translationRussian.put("тут", "here");
+
+        System.out.println(translation(translationRussian, TEXT));
+
+    }
+
+    private static StringBuffer translation(TreeMap<String, String> translationRussian, String text) {
+
+        String[] words = TEXT.toLowerCase().split(SPACE);
+        StringBuffer sb = new StringBuffer();
+
+        for(String word: words){
+            if(!translationRussian.containsKey(word)){
+                sb.append(translationRussian.get(word));
+                sb.append(SPACE);
+            } else {
+                sb.append(word);
+                sb.append(SPACE);
+            }
+        }
+
+        return sb;
+    }
+}
